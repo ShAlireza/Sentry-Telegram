9c434f6e5189beaa89decc483e094c0b670559cc
alireza <alirezashateri7@gmail.com>
2020-11-25 17:04:42 +0330

    add APi

--- apps/sentry/views.py
+++ apps/sentry/views.py
@@ -5,12 +5,19 @@ from _sha256 import sha256
 from django.conf import settings
 
 from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from .permissions import IsSentry
+from .models import Data
 
 
 class WebhookAPIView(GenericAPIView):
     permission_classes = (IsSentry,)
 
     def post(self, request):
-        pass
+        data = request.data
+
+        Data.objects.create(data=data)
+
+        return Response(status=status.HTTP_200_OK)
